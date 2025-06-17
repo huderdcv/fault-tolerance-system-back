11c964539220211be7e18c956cb8d355635f2ca4
huderdcv <huder.delacruz.27@unsch.edu.pe>
2025-06-16 23:59:24 -0500

    feat: agregar logica en el controlador para leer todos los estudiantes

--- src/presentation/students/controller.ts
+++ src/presentation/students/controller.ts
@@ -4,16 +4,19 @@ import { prisma } from '../../data/postgres';
 export class StudentsController {
   constructor() {}
 
-  public getStudents = (req: Request, res: Response) => {
-    res.json({
-      msg: 'Lista de estudiantes',
-    });
-    return;
+  public getStudents = async (req: Request, res: Response) => {
+    try {
+      const students = await prisma.student.findMany();
+      res.json(students);
+      return;
+    } catch (error) {
+      res.status(500).json({ error });
+      return;
+    }
   };
 
   public createStudent = async (req: Request, res: Response) => {
     const { nombre, email } = req.body;
-
     try {
       const newStudent = await prisma.student.create({
         data: { nombre, email },
