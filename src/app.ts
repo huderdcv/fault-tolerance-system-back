52144eebe1d28c67b484220c8ffbbd92add5cdf1
huderdcv <huder.delacruz.27@unsch.edu.pe>
2025-06-16 22:52:53 -0500

    feat: crear instancia de la clase Server para levantar el servidor

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
+import { envs } from './config/envs';
+import { AppRouter } from './presentation/routes';
+import { Server } from './presentation/server';
+
 (() => {
   main();
 })();
 
-function main() {}
+function main() {
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRouter.routes,
+  });
+
+  server.start();
+}
